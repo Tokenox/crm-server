08bcfecc567e07610f6989cf89d1bc9a1533461d
aliraza2u <raza8r@gmail.com>
2023-10-28 08:44:49 +0500

    define return types for dynamic table insertion

--- src/controllers/rest/DynamicController.ts
+++ src/controllers/rest/DynamicController.ts
@@ -1,11 +1,23 @@
-import { Controller } from "@tsed/di";
-import { BodyParams } from "@tsed/platform-params";
-import { Delete, Get, Post, Put } from "@tsed/schema";
+import { Controller, Inject } from "@tsed/di";
+import { BodyParams, Context } from "@tsed/platform-params";
+import { Delete, Get, Post, Put, Returns } from "@tsed/schema";
 import { Schema, model } from "mongoose";
+import { AdminService } from "../../services/AdminService";
+import { LeadService } from "../../services/LeadService";
 import { createSchema } from "../../helper";
+import { CategoryService } from "../../services/CategoryService";
+import { ADMIN, MANAGER } from "../../util/constants";
+import { BadRequest } from "@tsed/exceptions";
+import { CATEGORY_ALREADY_EXISTS } from "../../util/errors";
+import { SuccessResult } from "../../util/entities";
 
 @Controller("/dynamic")
 export class DynamicController {
+  @Inject()
+  private adminService: AdminService;
+  @Inject()
+  private categoryServices: CategoryService;
+
   @Post("/")
   async createDynamicModel(@BodyParams() modelData: any) {
     //! created schema for tables, which hold the ids of all the tables, we can create schema for that in the models folder and make relation with dynamic schema, model and table name
@@ -17,17 +29,23 @@ export class DynamicController {
 
   // insert data in dynamic schema and model
   @Post("/insert")
-  async insertDynamicModel(@BodyParams() modelData: any) {
+  @Returns(200, SuccessResult).Of(Object)
+  async insertDynamicModel(@BodyParams() modelData: any, @Context() context: Context) {
+    const { orgId } = await this.adminService.checkPermissions({ hasRole: [ADMIN, MANAGER] }, context.get("user"));
     const { tableName, columns, data } = modelData;
     const dynamicModel = createSchema({ tableName, columns });
+
+    const category = await this.categoryServices.findCategoryByNameAndOrgId({ name: tableName, orgId });
+    if (category) throw new BadRequest(CATEGORY_ALREADY_EXISTS);
+    const newCategory = await this.categoryServices.createCategory({ name: tableName, orgId });
     const newRecord = new dynamicModel({
       ...data,
       createdAt: new Date(),
       updatedAt: new Date(),
-      adminId: "5f9b3b7b9b0b3c2b3c2b3c2b"
+      category: newCategory?._id
     });
-    await newRecord.save();
-    return { message: `Model ${newRecord} created successfully.` };
+    const response = await newRecord.save();
+    return new SuccessResult(response, Object);
   }
 
   @Get("/")
