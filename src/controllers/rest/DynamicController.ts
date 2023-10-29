d403706a1f35b363a0bd956a587436accf3522d6
aliraza2u <raza8r@gmail.com>
2023-10-29 07:15:54 +0500

    created api for add bulk leads

--- src/controllers/rest/DynamicController.ts
+++ src/controllers/rest/DynamicController.ts
@@ -1,14 +1,14 @@
 import { Controller, Inject } from "@tsed/di";
-import { BodyParams, Context } from "@tsed/platform-params";
+import { BodyParams, Context, PathParams } from "@tsed/platform-params";
 import { Delete, Get, Post, Put, Returns } from "@tsed/schema";
 import { Schema, model } from "mongoose";
 import { AdminService } from "../../services/AdminService";
 import { LeadService } from "../../services/LeadService";
-import { createSchema } from "../../helper";
+import { createSchema, normalizeData } from "../../helper";
 import { CategoryService } from "../../services/CategoryService";
 import { ADMIN, MANAGER } from "../../util/constants";
 import { BadRequest } from "@tsed/exceptions";
-import { CATEGORY_ALREADY_EXISTS } from "../../util/errors";
+import { CATEGORY_ALREADY_EXISTS, CATEGORY_NOT_FOUND } from "../../util/errors";
 import { SuccessResult } from "../../util/entities";
 
 @Controller("/dynamic")
@@ -35,25 +35,64 @@ export class DynamicController {
     const { tableName, columns, data } = modelData;
     const dynamicModel = createSchema({ tableName, columns });
 
-    const category = await this.categoryServices.findCategoryByNameAndOrgId({ name: tableName, orgId });
-    if (category) throw new BadRequest(CATEGORY_ALREADY_EXISTS);
-    const newCategory = await this.categoryServices.createCategory({ name: tableName, orgId });
+    let category = await this.categoryServices.findCategoryByNameAndOrgId({ name: tableName, orgId });
+    if (!category) {
+      category = await this.categoryServices.createCategory({ name: tableName, orgId });
+    }
+
     const newRecord = new dynamicModel({
       ...data,
+      category: category?._id,
+      orgId,
       createdAt: new Date(),
-      updatedAt: new Date(),
-      category: newCategory?._id
+      updatedAt: new Date()
     });
     const response = await newRecord.save();
     return new SuccessResult(response, Object);
   }
 
-  @Get("/")
-  async getDynamicModel(@BodyParams() modelData: any) {
-    const { tableName, columns } = modelData;
+  // create endpoint to insert many records in dynamic schema and model
+  @Post("/createBulk")
+  @Returns(200, SuccessResult).Of(Object)
+  async insertManyDynamicModel(@BodyParams() modelData: any, @Context() context: Context) {
+    const { orgId } = await this.adminService.checkPermissions({ hasRole: [ADMIN, MANAGER] }, context.get("user"));
+    const { tableName, columns, data } = modelData;
     const dynamicModel = createSchema({ tableName, columns });
-    const result = await dynamicModel.find({});
-    return result;
+
+    let category = await this.categoryServices.findCategoryByNameAndOrgId({ name: tableName, orgId });
+    if (!category) {
+      category = await this.categoryServices.createCategory({ name: tableName, orgId });
+    }
+    // format data to insert in dynamic schema
+    const formattedData = data.map((item: any) => {
+      return {
+        ...item,
+        orgId,
+        categoryId: category?._id.toString(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+    });
+    const response = await dynamicModel.insertMany(formattedData);
+    return new SuccessResult(normalizeData(response), Object);
+  }
+
+  @Get("/:categoryId")
+  @Returns(200, SuccessResult).Of(Object)
+  async getDynamicModel(@PathParams("categoryId") categoryId: string, @Context() context: Context) {
+    const { orgId } = await this.adminService.checkPermissions({ hasRole: [ADMIN, MANAGER] }, context.get("user"));
+    const category = await this.categoryServices.findCategoryById(categoryId);
+    if (!category) throw new BadRequest(CATEGORY_NOT_FOUND);
+    const tableName = category.name;
+    let dynamicModel;
+    try {
+      dynamicModel = model(tableName);
+    } catch (error) {
+      const ProductSchema = new Schema({}, { strict: false });
+      dynamicModel = model(tableName, ProductSchema);
+    }
+    const result = await dynamicModel.find();
+    return new SuccessResult(normalizeData(result), Object);
   }
 
   @Put("/update")
--- src/util/errors.ts
+++ src/util/errors.ts
@@ -25,6 +25,7 @@ export const SESSION_ALREADY_EXISTS = "SESSION_ALREADY_EXISTS";
 export const ORGANIZATION_NAME_ALREADY_EXISTS = "ORGANIZATION_NAME_ALREADY_EXISTS";
 export const ADMIN_ALREADY_EXISTS = "ADMIN_ALREADY_EXISTS";
 export const CATEGORY_ALREADY_EXISTS = "CATEGORY_ALREADY_EXISTS";
+export const CATEGORY_NOT_FOUND = "CATEGORY_NOT_FOUND";
 
 export const errorMap: { [key: string]: string } = {
   SOMETHING_WENT_WRONG: "Looks like something is wrong, we are working to fix it.",
@@ -54,5 +55,6 @@ export const errorMap: { [key: string]: string } = {
   SESSION_ALREADY_EXISTS: "You have already logged in from another device.",
   ORGANIZATION_NAME_ALREADY_EXISTS: "Organization already exists with this name.",
   ADMIN_ALREADY_EXISTS: "Admin already exists with this email.",
-  CATEGORY_ALREADY_EXISTS: "Category already exists with this name."
+  CATEGORY_ALREADY_EXISTS: "Category already exists with this name.",
+  CATEGORY_NOT_FOUND: "Category not found."
 };
