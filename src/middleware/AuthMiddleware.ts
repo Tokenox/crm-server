69afd5e2c37c7c5dcb097269e866fd5924d2f72a
aliraza2u <raza8r@gmail.com>
2024-06-11 12:39:18 +0500

    fix issues

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -13,8 +13,6 @@ export class AuthMiddleware {
   public adminService: AdminService;
 
   public async use(@Req() req: Req, @Context() ctx: Context) {
-    const verify = await verification();
-    if (!verify.isVerify) throw new Forbidden(INVALID_TOKEN);
     const isPublicRoute = ctx.request.url.startsWith("/docs") || ctx.request.url.startsWith("/rest/auth");
     //  || ctx.request.url.startsWith("/rest/webhook");
     const adminToken = req?.headers?.authorization || req.headers.cookie?.split("session=")[1];
